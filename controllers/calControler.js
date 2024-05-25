64c9dd11c5aead6f0a798a08d461cb3bc133f6b9
kevin ponce <kevin.ponce@ieesa.edu.hn>
2024-05-25 16:56:19 -0600

    v 1.0.1

--- controllers/calControler.js
+++ controllers/calControler.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const requestDataModel = require("../models/requestDataModel");
 const ComboModel = require("../models/comboModel");
+const { json } = require('body-parser');
 
 class Calcular {
     async hacerCalculos(req, res, informeVentas) {
@@ -11,38 +12,48 @@ class Calcular {
                 res.redirect("/");
                 return;
             }
-            
+            let totalGen = 0;
             // Filtrar los combos
             const combos = informeVentas.filter(item => item.producto.toUpperCase().includes('COMBO'));
-            
             // Eliminar los combos del informe original
             const informeSinCombos = informeVentas.filter(item => !item.producto.toUpperCase().includes('COMBO'));
-            const resMat = [];
+            const resMat = [];//matris para guardar los resultados
+            //for para hacer los calculos de cada producto
             for (const data of formData) {
-                const obj = { ...data };
+                const obj = { ...data };//copia los datos a un objecto
+                //obtiene los datos referentes al producto en informe de ventas
                 const infoVentas = informeSinCombos.find(item => item.producto.toUpperCase() === obj.productName.toUpperCase());
-                if (infoVentas) {
+                if (infoVentas) {//si se optienen los datos
                     obj.TotalProductos = obj.inventoryInitial + obj.entrada - obj.salida;
-                    obj.inventoryFinal = obj.TotalProductos - infoVentas.cantidad;
+                    obj.inventarioFinal = obj.TotalProductos - infoVentas.cantidad;
                     const modeloCombos = new ComboModel();
                     let balanceCombos = 0
                     for (const combo of combos) {
                         const result = await modeloCombos.comboIncludeProduct([obj.productName,combo.producto]);
                         //console.log(result.rows[0][0].Cantidad_Producto)
-                        
+                        //si el combo si contiene ese producto
                         if (result.rows[0].length >0) {
                             //combo.cantidad = la cantidad de ese combo que se vendio
                             //y rows.cantidad_producto = la cantidad de ese producto que incluye el combo
                             balanceCombos+= (combo.cantidad*result.rows[0][0].Cantidad_Producto)
                         }
                     }
+                     //calcula el total del producto 
+                     obj.totalIndi = infoVentas.importe * infoVentas.cantidad;
+                    totalGen+= obj.totalGen;
+                    obj.balanceInventario = data.inventarioReal-obj.inventarioFinal
                     obj.balanceCombos = balanceCombos
                 }
                 resMat.push(obj);
             }
-
-            // Responder al cliente con un mensaje de confirmación
+            
+            const modeloCombos = new ComboModel();
+            const jsonData = JSON.stringify([resMat]); // Asumiendo que resMat es un objeto o array que quieres convertir a JSON
+            const idEmpleado = 1;
+            console.log(jsonData)
+            modeloCombos.sendInfoVentaJson([jsonData, idEmpleado])
             console.log(resMat)
+            // Responder al cliente con un mensaje de confirmación
             return res.json({ status: 'Datos recibidos correctamente.', resMat });
         } catch (error) {
             console.error('Error al obtener los datos:', error);
--- models/comboModel.js
+++ models/comboModel.js
@@ -7,9 +7,25 @@ class ComboModel {
         const [comboName, productName] = params;
         const data = await conexion.queryParams("CALL validarProductoYCombo(?,?)", [comboName, productName]);
         await conexion.desconectar();
-        //console.log("test",data.rows)
         return data;
     }
+
+    //sube a la base de datos el informe de venta 
+    async sendInfoVentaJson(params) {
+        const conexion = new ConexionClass();
+        try {
+            await conexion.conectar();
+            const query = "INSERT INTO historia_informes (json_body_historial, id_empleados) VALUES (?, ?)";
+            const data = await conexion.queryParams(query, params);
+            return data;
+        } catch (error) {
+            console.error("Error al ejecutar la consulta:", error);
+            throw error;
+        } finally {
+            await conexion.desconectar();
+        }
+    }
+    
 }
 
 module.exports = ComboModel;
